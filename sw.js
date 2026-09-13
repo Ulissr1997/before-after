763bbbba4b8abe4c2907c908cb0121ef9eb9d6a6
Aldo Shkurta <85101016+Ulissr1997@users.noreply.github.com>
2026-09-14 00:07:11 +0200

    Preview of videos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'before-after-v19';
+const CACHE = 'before-after-v24';
 const FONT_CACHE = 'before-after-fonts';
 const ASSETS = ['./', './index.html', './video.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png',
   './inter-400.woff2', './inter-600.woff2', './anton-400.woff2', './playfair-700.woff2', './caveat-700.woff2'];
